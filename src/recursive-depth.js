ce5ec39c516a556f3cf5732cc0653dc47e62467c
Your Name <you@example.com>
2021-09-13 23:46:27 +0300

    feat: add recursive-depth function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,11 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,8 +13,17 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    var testing = 0;
+    if (Array.isArray(arr)) {
+      if (arr.length == 0) {
+        testing = 1;
+      } else {
+        testing = 1 + Math.max(...arr.map(this.calculateDepth.bind(this)));
+      }
+    } else testing = 0;
+
+    console.log("res", testing);
+    return testing;
   }
 }
