28fde436f98331394e80e121f686c19023bdb924
Your Name <you@example.com>
2021-09-30 23:48:06 +0300

    feat: add extended-repeater feat: add vigenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,51 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options = null) {
+    if (options == null) { return (str); }
+    var result = "";
+    if (typeof options.repeatTimes == "undefined") {
+        options.repeatTimes = 1;
+    };
+    if (typeof options.additionRepeatTimes == "undefined") {
+        options.additionRepeatTimes = 1;
+    };
+    if (typeof options.separator == "undefined") {
+        options.separator = '+';
+    };
+    if (typeof options.addition == "undefined") {
+        options.addition = '';
+    };
+    if (typeof options.additionSeparator == "undefined") {
+        options.additionSeparator = '|';
+    };
+    for (let i = 0; i < options.repeatTimes; i++) {
+        result = result + str;
+
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+            result = result + options.addition;
+            if (j < options.additionRepeatTimes - 1) {
+                result = result + options.additionSeparator;
+            }
+        }
+        if (i < options.repeatTimes - 1) {
+            result = result + options.separator;
+        }
+    }
+
+    return result;
 }
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,63 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    constructor(direction = true) {
+        this.direction = direction;
+    }
+
+    encrypt(message, key) {
+        if (typeof message == "undefined" || typeof key == "undefined") {
+            throw Error("Incorrect arguments!");
+        }
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        var encrResult = this.Vizhener(message, key, "encrypt");
+        if (!this.direction) { return encrResult.split("").reverse().join(""); }
+        return (encrResult);
+
+    }
+    decrypt(message, key) {
+        if (typeof message == "undefined" || typeof key == "undefined") {
+            throw Error("Incorrect arguments!");
+        }
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        var decrResult = this.Vizhener(message, key, "decrypt");
+
+        if (!this.direction) { return decrResult.split("").reverse().join(""); }
+        return (decrResult);
+
+
+    }
+
+    Vizhener(m, k, mode) {
+        var a = this.a;
+
+        var r = ''; //Пустой результат
+        var c;
+        var j = 0;
+        for (let i = 0; i < m.length; i++) { //encrypt/decrypt
+            //Vizhener - encrypt/decrypt one forumula (encrypt - by default; decrypt - when (mode === 'decrypt') )
+
+            var mi = a.indexOf(m[((i >= m.length) ? i % m.length : i)]); //подгон сообщения/шифротекста - к ключу (если меньше)
+
+            if (mi == -1) {
+                r += m[i];
+
+                continue;
+            }
+            var ki_s = k[((j >= k.length) ? j % k.length : j)];
+            //подгон ключа к сообщению/шифротексту (если короткий)
+            var ki = (typeof mode !== 'undefined' && mode.indexOf('gronsfeld') !== -1) ? parseInt(ki_s) : a.indexOf(ki_s);
+            //вычитание при дешифровании, либо сложение.
+            ki = ((typeof mode !== 'undefined' && mode.indexOf('decrypt') !== -1) ? (-ki) : ki);
+            c = a[(((a.length + (mi + ki)) % a.length))]; //символ по таблице Виженера.
+            c = (mode === 'shifted_atbash') ? a[a.length - 1 - a.indexOf(c)] : c; //Атбаш символа или символ.
+            r += c; //Добавить символ к результату.
+            j++;
+        }
+        return r; //вернуть строку результата
+
+    }
 }
